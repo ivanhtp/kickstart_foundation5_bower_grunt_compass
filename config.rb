86dd4f0307f9ef522c4e1aacede54fab5b5b2728
Ivan Pauletti <ivanpauletti@gmail.com>
2014-01-29 01:03:21 -0200

    Foundation + Bower!

--- config.rb
+++ config.rb
@@ -1,12 +1,14 @@
 # Require any additional compass plugins here.
 add_import_path "bower_components/foundation/scss"
 
+
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "development/css"
 sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "img"
+javascripts_dir = "development/lib"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
